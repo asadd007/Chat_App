8d196ecb7bb32deb8943e8aea381e2e20036cfaa
asadd007 <asadkhan05547@gmail.com>
2022-03-26 10:04:28 +0530

    Created page of chatengine

--- chat_app/chat_app/src/App.js
+++ chat_app/chat_app/src/App.js
@@ -1,9 +1,17 @@
-import { ChatEngine } from 'react-chat-engine';
+import React from "react";
+import {ChatEngine} from "react-chat-engine" 
 import './App.css';
-const App = () => {
-  return (
-    <ChatEngine/>
-  );
-};
 
+function App() {
+    return(
+       <div className="App">
+           <ChatEngine
+               height="100vh"
+               projectID="26c690e0-5665-45ad-8be4-da5aeeb70ce3"
+               userName="Asad"
+               userSecret="asadkhan"
+           />
+       </div>
+    );
+}
 export default App;
